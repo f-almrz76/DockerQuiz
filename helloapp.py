c082d5a4ae896f1252eb7a1ad3d8d7080ddd4e16
minatests <iminafattahi@gmail.com>
2023-09-19 19:30:56 +0330

    feat: app dockerized

--- helloapp.py
+++ helloapp.py
@@ -1,3 +1,13 @@
-name = input("What's your name?")
+from flask import Flask
 
-print(f"Hello {name}...")
+app = Flask(__name__)
+
+
+name = input("What's your name?")
+@app.route("/")
+def say_hello(name):
+    return f"Hello {name}..."
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
