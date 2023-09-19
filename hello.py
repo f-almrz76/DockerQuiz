8e837a96bb8390d53a3e1c369a702afb9ca04f3e
Ali Afzal <aliakbar_afzal@yahoo.com>
2023-09-19 19:52:29 +0330

    fix:fix hello.py

--- hello.py
+++ hello.py
@@ -2,16 +2,9 @@ name = input("Enter your name: ")
 print(f"Hello, {name}!")
 
 
-# Dockerfile:
-FROM python:3.8-slim
 
-WORKDIR /app
-
-COPY hello.py .
-
-CMD ["python", "hello.py"]
 
 # Terminal:
-docker build -t hello-app .
+# docker build -t hello-app .
 
-docker run -it --rm hello-app
+# docker run -it --rm hello-app
