d0306e3a0e0ed72ac279a995f079487ab695ef19
minatests <iminafattahi@gmail.com>
2023-09-19 19:50:36 +0330

    done with my tests this is the actual quiz

--- helloapp.py
+++ helloapp.py
@@ -1,14 +1,3 @@
-from flask import Flask
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def say_hello(name):
-    
-    name = request.args.get("name")
-    print(f"Hello {name}...")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+name = input("What's your name?")
+print(f"Hello {name}...")
